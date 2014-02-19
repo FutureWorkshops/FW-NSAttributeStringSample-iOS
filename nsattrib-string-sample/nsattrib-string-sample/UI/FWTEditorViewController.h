dad974b1395f147c03a0f5393a2dec51dc8c29fc
Carlos Vidal <carlos@Carlos-iMac.local>
2014-02-19 16:27:18 +0000

    created editor view controller class

---
+++ nsattrib-string-sample/nsattrib-string-sample/UI/FWTEditorViewController.h
@@ -0,0 +1,13 @@
+//
+//  FWTEditorViewController.h
+//  nsattrib-string-sample
+//
+//  Created by Carlos Vidal on 19/02/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWTEditorViewController : UIViewController
+
+@end
---
+++ nsattrib-string-sample/nsattrib-string-sample/UI/FWTEditorViewController.m
@@ -0,0 +1,38 @@
+//
+//  FWTEditorViewController.m
+//  nsattrib-string-sample
+//
+//  Created by Carlos Vidal on 19/02/2014.
+//
+//
+
+#import "FWTEditorViewController.h"
+
+@interface FWTEditorViewController ()
+
+@end
+
+@implementation FWTEditorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
