c34d03abea30c3d4086f6d0ee962af4f62edde9d
Carlos Vidal <carlos@Carlos-iMac.local>
2014-02-19 17:25:57 +0000

    added syntax button to editor view controller

--- nsattrib-string-sample/nsattrib-string-sample/UI/FWTEditorViewController.m
+++ nsattrib-string-sample/nsattrib-string-sample/UI/FWTEditorViewController.m
@@ -7,32 +7,85 @@
 //
 
 #import "FWTEditorViewController.h"
+#import "FWTWebViewController.h"
+#import "FWTHTMLSyntax.h"
 
-@interface FWTEditorViewController ()
+@interface FWTEditorViewController () <UITextViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UITextView *editorTextView;
+@property (nonatomic, weak) IBOutlet UITextView *previewerTextView;
 
 @end
 
 @implementation FWTEditorViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    
+    [self _setupTextViews];
+    [self _setupNavigationBar];
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - Private methods
+- (UIColor*)_borderColor
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    return [UIColor lightGrayColor];
+}
+
+- (void)_setupNavigationBar
+{
+    UIBarButtonItem *clearButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Clear" style:UIBarButtonItemStyleDone target:self action:@selector(_clearText)];
+    self.navigationItem.rightBarButtonItem = clearButtonItem;
+    
+    UIBarButtonItem *aboutButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"About" style:UIBarButtonItemStylePlain target:self action:@selector(_presentAbout)];
+    self.navigationItem.leftBarButtonItem = aboutButtonItem;
+}
+
+- (void)_setupTextViews
+{
+    [self.editorTextView.layer setBorderWidth:1.f];
+    [self.editorTextView.layer setBorderColor:[self _borderColor].CGColor];
+    
+    [self _renderPlainText:self.editorTextView.text];
+}
+
+- (void)_clearText
+{
+    self.editorTextView.text = @"";
+    self.previewerTextView.text = @"";
+}
+
+- (void)_renderPlainText:(NSString*)plainText
+{
+    NSMutableAttributedString *attributedProductDescription = [[NSMutableAttributedString alloc] initWithData:[plainText dataUsingEncoding:NSUnicodeStringEncoding]
+                                                                                                      options:@{NSDocumentTypeDocumentAttribute:NSHTMLTextDocumentType}
+                                                                                           documentAttributes:nil
+                                                                                                        error:nil];
+    [self.previewerTextView setAttributedText:attributedProductDescription];
+}
+
+- (IBAction)_presentHTMLSyntax:(id)sender
+{
+    [self.editorTextView setText:kSyntax];
+    [self _renderPlainText:kSyntax];
+}
+
+- (void)_presentAbout
+{
+    FWTWebViewController *webViewController = [[FWTWebViewController alloc] init];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:webViewController];
+    [self.navigationController presentViewController:navigationController animated:YES completion:nil];
+}
+
+#pragma mark - UITextFieldDelegate methods
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
+    NSString *newString = [textView.text stringByReplacingCharactersInRange:range withString:text];
+    
+    [self _renderPlainText:newString];
+    
+    return YES;
 }
 
 @end
