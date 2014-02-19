910f3511beb06283507982fe6dd98eceaf101af8
Carlos Vidal <carlos@Carlos-iMac.local>
2014-02-19 17:20:35 +0000

    created syntax header file

---
+++ nsattrib-string-sample/nsattrib-string-sample/FWTHTMLSyntax.h
@@ -0,0 +1,13 @@
+//
+//  FWTHTMLSyntax.h
+//  nsattrib-string-sample
+//
+//  Created by Carlos Vidal on 19/02/2014.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol FWTHTMLSyntax <NSObject>
+
+@end
